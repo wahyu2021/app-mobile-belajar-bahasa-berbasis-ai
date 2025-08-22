adb563b9959b45cdf1881de2b79e6d01b1ee3261
wahyu2021 <wahyu256@gmail.com>
2025-08-23 01:48:51 +0700

    pusing

--- api_fastapi/app/models/associations.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Table, Column, String, Integer, ForeignKey
-from ..database import Base
-
-level_item_association = Table(
-    'level_item_mappings', Base.metadata,
-    Column('level_id', String(50), ForeignKey('levels.id'), primary_key=True),
-    Column('item_id', Integer, ForeignKey('items.id'), primary_key=True)
-)
